524ff00f964fe87a2419526625f9263719b8928f
Dina Deifallah <dinadeif@gmail.com>
2022-02-15 12:17:16 +0100

    Update main.py
    fixing syntax error in main

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ from recommender import recommend_random
 movies = pd.read_csv('data/movies.csv')
 recommendations = recommend_random(movies=movies)
 assert len(recommendations)==10
-  print(recommendations)
+print(recommendations)
