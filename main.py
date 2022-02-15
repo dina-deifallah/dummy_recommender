ff2f0e6576e87565f563113b596d100ddd48e734
dina-deifallah <dinadeif@gmail.com>
2022-02-15 11:45:09 +0100

    adding program files

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+main program for dummy recommender
+'''
+
+import pandas as pd
+from recommender import recommend_random
+
+
+
+movies = pd.read_csv('data/movies.csv')
+recommendations = recommend_random(movies=movies)
+assert len(recommendations)==10
+print(recommendations)
