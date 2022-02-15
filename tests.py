41ed018ad808163bb767bf1662c66511e95c7ff1
Dina Deifallah <dinadeif@gmail.com>
2022-02-15 13:12:23 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,11 @@
-from recommender import recommend_random
+"module containing all tests on my recommender"
+
 import pandas as pd
+from recommender import recommend_random
 
 # test recommend_random function
 def test_recommender():
+    "testing that my recommender returns the default k recommendations"
     movies = pd.read_csv('data/movies.csv')
     recommendations = recommend_random(movies=movies)
     assert len(recommendations)==10
