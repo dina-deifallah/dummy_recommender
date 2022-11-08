2e2639b7b35066919eefd7cbf0125e7a6a810ee7
Dina Deifallah <dinadeif@gmail.com>
2022-11-08 12:48:22 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 "tests for my application"
 
 def test_always_passes():
+    "dummy test that always passes"
     assert True
 
 def test_always_fails():
+    "dummy test that always fails"
     assert False
